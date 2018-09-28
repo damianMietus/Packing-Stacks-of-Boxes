28b403f2e708b1a2920da35c35ad26067a9a07a8
damianMietus <damianmietus1@gmail.com>
2018-09-28 09:30:07 -0400

    Add files via upload
    Added the .cpp that contains the foundation to the solution.

---
+++ box.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <list>
+using namespace std;
+
+int main()
+{
+
+    int boxArraySize = 1;
+    int numOfStacks = 0;
+    int i = 0;
+    int j = 0;
+    int temp = 0;
+    string str;
+    
+    cout << "Input the number stacks followed by the boxes: ";
+    getline (cin, str);
+    if (isdigit(str[0]) && str[1] == ' '){
+        cout << "This is valid input!\n";
+        j = 0;
+        numOfStacks = str[0];
+        boxArraysize = str.length() - 1;
+        int *boxArray = new int[boxArraySize];
+        for (i = 2; i < 0 + str.length(); i++){
+        
+            temp = str[i] - '0';
+            boxArray[j] = temp;
+            cout << boxArray[j] << " Is in the box array!\n";
+            j++;
+        
+        }
+        
+        
+    } else {
+     
+        cout << "Invalid input. Try re-running the program";
+        return 0;
+        
+    }
+}
