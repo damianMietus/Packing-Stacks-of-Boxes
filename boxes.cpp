3bc7fad459a76713bc49e962b0207620006aea95
damianMietus <damianmietus1@gmail.com>
2018-10-11 09:41:11 -0400

    Update boxes.cpp
    Got it to work without pointers or lists, but lost modular functions to do so.

--- boxes.cpp
+++ boxes.cpp
@@ -19,13 +19,14 @@ typedef struct STACK {
     int seven;
     int eight;
     int nine;
+    bool increment;
 } Stack;
 
 void displayArray(int array[], int arraySize);
 void failure();
-Stack* addToStack(Stack stacks[], string str);
-Stack addNumbersToStack(Stack stack);
-
+Stack* addToStack(Stack stacks[], string str, int indexToAdd);
+Stack addNumberToStack(Stack stack, string number);
+bool checkStackForDuplicate(Stack stack[], Stack temp, int end);
 
 
 int main()
@@ -42,7 +43,8 @@ int main()
     string boxStr;
     string tempStr;
     
-   // bool firstFlag = true;
+    bool duplicateFlag = true;
+    bool firstFlag = true;
     
     list<int> permuList;
     
@@ -79,8 +81,8 @@ int main()
         cout << boxStr << endl;
         
         string tempStr;
-        Stack stackArray[stackSize*10];
-        int numbOfStacks = 0;
+        Stack stackArray[10 * numOfStacks];
+        int numbOfStacks = 1;
         
         // Get the permutations of the string to find possible stacks
         do {
@@ -99,10 +101,76 @@ int main()
                 }
                 
                 if (temp == stackSize){
-                    cout << "Found a possible stack : " << tempStr << endl;
+                    //cout << "Found a stack : " << tempStr << endl;
                     
-                    //add to list
-                    //stackArray[numbOfStacks]
+                    //add to list]
+                   // stackArray[numbOfStacks] = addToStack(stackArray, tempStr, numbOfStacks);
+                   
+                   Stack temp;
+                   temp.str = tempStr;
+                   temp.zero = 0;
+                   temp.one = 0;
+                   temp.two = 0;
+                   temp.three = 0;
+                   temp.four = 0;
+                   temp.five = 0;
+                   temp.six = 0;
+                   temp.seven = 0;
+                   temp.eight = 0;
+                   temp.nine = 0;
+                   //Put numbers in this stack
+                    for (i = 0; i < temp.str.length() ; i++){
+                        if (temp.str[i] == '0'){
+                            temp.zero++;
+                        } else if (temp.str[i] == '1'){
+                            temp.one++;
+                        } else if (temp.str[i] == '2'){
+                            temp.two++;
+                        } else if (temp.str[i] == '3'){
+                            temp.three++;
+                        } else if (temp.str[i] == '4'){
+                            temp.four++;
+                        } else if (temp.str[i] == '5'){
+                            temp.five++;
+                        } else if (temp.str[i] == '6'){
+                            temp.six++;
+                        } else if (temp.str[i] == '7'){
+                            temp.seven++;
+                        } else if (temp.str[i] == '8'){
+                            temp.eight++;
+                        } else if (temp.str[i] == '9'){
+                            temp.nine++;
+                        } else {
+                            exit (EXIT_FAILURE);
+                        }
+                    }
+                   
+                    if (firstFlag == true){
+                        stackArray[0] = temp;
+                        firstFlag = false;
+                        cout << "The first one: " << stackArray[0].str << endl;
+                    } else {
+                        // check for duplicate stacks
+                        duplicateFlag = false;
+                        for (i = 0; i < numbOfStacks+1; i ++){
+        
+                            if ( (stackArray[i].zero == temp.zero) && (stackArray[i].one == temp.one) 
+                            && (stackArray[i].two == temp.two) && (stackArray[i].three == temp.three)
+                            && (stackArray[i].four == temp.four) && (stackArray[i].five == temp.five)
+                            && (stackArray[i].six == temp.six) && (stackArray[i].seven == temp.seven)
+                            && (stackArray[i].eight == temp.eight) && (stackArray[i].nine == temp.nine) ){
+                                duplicateFlag = true;  
+                                //cout << "D'oh! It already exists! xD" << endl;
+                            }
+    
+                        }
+                        if (duplicateFlag == false){
+                            stackArray[numbOfStacks] = temp;
+                            cout << "String: " << stackArray[numbOfStacks].str << " put in this index: " << numbOfStacks << endl;
+                            numbOfStacks++;
+                        } // else it already exists. Change nothing
+                        
+                    }  
                     
                 }
                       
@@ -131,21 +199,79 @@ void failure(){
     exit (EXIT_FAILURE);
 }
 
-Stack* addToStack(Stack stacks[], string str){
+/*
+
+Stack* addToStack(Stack stacks[], string str, int indexToAdd){
 
     Stack temp;
     temp.str = str;
+    temp = addNumberToStack(temp, str);
+    stacks[0].increment = false;
     
     if (stacks == 0){
+        stacks[indexToAdd] = temp;
+        stacks[0].increment = true;
+    } else {
         
+        if ( checkStackForDuplicate(stacks, temp, indexToAdd) == true){
+            // No duplicates
+            stacks[indexToAdd] = temp;
+            stacks[0].increment = true;
+        } else {
+            // already exists. Ignore this!
+            return stacks;
+        }
     }
 
     return stacks;
 
 }
-/*
-Stack addNumbersToStack(Stack stack){
 
+Stack addNumberToStack(Stack stack, string number){
+    int i = 0;
+    for (i = 0; i < number.length() ; i++){
+        if (number[i] == '0'){
+            stack.zero++;
+        } else if (number[i] == '1'){
+            stack.one++;
+        } else if (number[i] == '2'){
+            stack.two++;
+        } else if (number[i] == '3'){
+            stack.three++;
+        } else if (number[i] == '4'){
+            stack.four++;
+        } else if (number[i] == '5'){
+            stack.five++;
+        } else if (number[i] == '6'){
+            stack.six++;
+        } else if (number[i] == '7'){
+            stack.seven++;
+        } else if (number[i] == '8'){
+            stack.eight++;
+        } else if (number[i] == '9'){
+            stack.nine++;
+        } else {
+            exit (EXIT_FAILURE);
+        }
+    }
+    
+    return stack;
+}
 
+bool checkStackForDuplicate(Stack stack[], Stack temp, int end){
+    int i = 0;
+    for (i = 0; i < end; i ++){
+        
+        if ( (stack[i].zero == temp.zero) && (stack[i].one == temp.one) 
+        && (stack[i].two == temp.two) && (stack[i].three == temp.three)
+        && (stack[i].four == temp.four) && (stack[i].five == temp.five)
+        && (stack[i].six == temp.six) && (stack[i].seven == temp.seven)
+        && (stack[i].eight == temp.eight) && (stack[i].nine == temp.nine) ){
+            return false;
+        }
+    
+    }
+    return true;
+}
 
-}*/
+*/
